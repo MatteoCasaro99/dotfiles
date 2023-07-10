94d1c16670e71096d14d36305719b60e742e6acb
ZioPeppina <matteo.casaro99@gmail.com>
2023-07-10 21:25:35 +0200

    Removed coc folder, nvim commands for window management added

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,5 +1,5 @@
 local options = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 
 -- space as leader key (first line unmaps space, the others map it to leader)
@@ -13,9 +13,21 @@ keymap("n", "<leader>O", ":<C-u>put!=repeat(nr2char(10),v:count)<Bar>execute \"'
 
 
 -- <leader>pv : opens netrw (:Ex)
-keymap("n", "<leader>pv", ":Explore<CR>", options)
+keymap("n", "<leader>pv", ":Lexplore<CR>", options)
 
 
 -- maintain page centered when scrolling via C-d or C-u
 keymap("n", "<C-d>", "<C-d>zz", options)
 keymap("n", "<C-u>", "<C-u>zz", options)
+
+
+-- open and close splits with <leader>w[v/h/q]
+keymap("n", "<leader>wv", ":vnew<CR>", options)
+keymap("n", "<leader>wh", ":new<CR>", options)
+keymap("n", "<leader>wq", "<C-w>q", options)
+
+-- move around windows with Alt-h/j/k/l
+keymap("n", "<A-h>", "<C-w>h", options)
+keymap("n", "<A-j>", "<C-w>j", options)
+keymap("n", "<A-k>", "<C-w>k", options)
+keymap("n", "<A-l>", "<C-w>l", options)
